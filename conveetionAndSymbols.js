2f1082950ec8b34c9f07bf4a45903123553fcdc8
Malik Awais <malikawais@example.com>
2025-09-24 16:12:55 +0500

    UPDATE CONVERSION

--- conveetionAndSymbols.js
+++ conveetionAndSymbols.js
@@ -14,5 +14,33 @@ let ConverstionOfboolean = Boolean(value)
 
 let num = "1"
 let ConverstionOfString = String(num)
-console.log( typeof ConverstionOfString)
+//console.log( typeof ConverstionOfString)
 
+// 1. Number (int) to String
+let  num1 = "21"
+let NubIntoString = String (num1) 
+console.log("number to string",NubIntoString,"type",typeof NubIntoString);
+
+//String to Number
+
+let num2 = "31";
+let StringToNumber = Number(num2)
+
+console.log("string to number",StringToNumber,"type:", typeof StringToNumber);
+
+// 3. String to Boolean
+let namesss= "awais"
+let StringtoBoolean =  Boolean(namesss)
+console.log("string to boolean",StringtoBoolean,"types:", typeof StringtoBoolean);
+
+// 4. Boolean to String
+let names1 = "true"
+let BooleanToString = String(names1)
+console.log("boolean to string",BooleanToString,"types:", typeof BooleanToString);
+
+
+ // 5. Convert "0" into a boolean
+ let zero= "0"
+ let ZeroToBoolean = Boolean (Number (zero))
+ console.log("zero to boolean",ZeroToBoolean,"type:", typeof ZeroToBoolean);
+ 
