4b6a60bbdb863bb888b080b276b03a94fef7e78a
Malik Awais <malikawais@example.com>
2025-09-24 15:41:21 +0500

    converstionAndSymbols

---
+++ conveetionAndSymbols.js
@@ -0,0 +1,18 @@
+let score = null
+
+//console.log(typeof score);
+
+let ConverstionOfNumber = Number(score)
+//console.log(  ConverstionOfNumber);
+
+
+
+let value = "awais"
+let ConverstionOfboolean = Boolean(value)
+//console.log(ConverstionOfboolean);
+
+
+let num = "1"
+let ConverstionOfString = String(num)
+console.log( typeof ConverstionOfString)
+
