da72fa18be29e7a7f47a419cae77e141b52c7bdc
Malik Awais <malikawais@example.com>
2025-09-24 14:45:46 +0500

    config

---
+++ test.js
@@ -0,0 +1,15 @@
+"use strict";
+console.log("awais");
+const namees = "awais";
+
+ 
+ //
+ //namees = ("awais");
+
+ const age= 34;
+ console.log(namees,age);
+ 
+// console.log(typeof age);
+console.table([typeof age, namees]);
+
+alert (3+3 );
