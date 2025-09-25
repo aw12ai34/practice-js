4e5b3f4c160bd4f7559bbf58c3f539b681542ddf
Malik Awais <malikawais@example.com>
2025-09-25 15:46:11 +0500

    conventionOrOpertation

---
+++ conveetionAndOperation.js
@@ -0,0 +1,86 @@
+let score = null
+
+//console.log(typeof score);
+
+let ConverstionOfNumber = Number(score)
+//console.log(  ConverstionOfNumber);
+
+
+
+let value = "awais"
+let ConverstionOfboolean = Boolean(value)
+//console.log(ConverstionOfboolean);
+
+
+let num = "1"
+let ConverstionOfString = String(num)
+//console.log( typeof ConverstionOfString)
+
+// 1. Number (int) to String
+let  num1 = "21"
+let NubIntoString = String (num1) 
+//console.log("number to string",NubIntoString,"type",typeof NubIntoString);
+
+//String to Number
+
+let num2 = "31";
+let StringToNumber = Number(num2)
+
+//console.log("string to number",StringToNumber,"type:", typeof StringToNumber);
+
+// 3. String to Boolean
+let namesss= "awais"
+let StringtoBoolean =  Boolean(namesss)
+//console.log("string to boolean",StringtoBoolean,"types:", typeof StringtoBoolean);
+
+// 4. Boolean to String
+let names1 = "true"
+let BooleanToString = String(names1)
+//console.log("boolean to string",BooleanToString,"types:", typeof BooleanToString);
+
+
+ // 5. Convert "0" into a boolean
+ let zero= "0"
+ let ZeroToBoolean = Boolean (Number (zero))
+ //console.log("zero to boolean",ZeroToBoolean,"type:", typeof ZeroToBoolean);
+ 
+
+
+ // 1. Number (int) to String
+
+ let numb ="31"
+ let NumberToString = String(numb)
+ //console.log(typeof NumberToString);
+ 
+
+
+// *******************************operation***********************
+ let numbe = 1 / 1
+ //console.log(numbe);
+
+ let str1 = "awais"
+ let str2 = " computer Engineer"
+ let str3 = str1 + str2
+ //console.log(str3); 
+//   console.log( 1+2 );
+//     console.log( 1+"2" );
+    //  console.log( 1+2+7+ "2");
+        //console.log( "9"+1+2 )
+        //console.log(true);
+
+
+
+  let  awais  = 4
+
+//   console.log(awais);
+  
+ //console.log(+true);
+//   console.log(+"");
+
+let game = 1002
+--game; // postfix
+//console.log(game);
+
+   let game1 = 1002
+game1 --; // postfix
+console.log(game);
